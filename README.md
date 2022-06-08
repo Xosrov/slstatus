a3e4e67deb1d696511d6442706943ab0f26f8ed9
Alireza <seyed.alireza.miryazdi@gmail.com>
2022-06-09 02:58:09 +0430

    added note about void linux

---
+++ README.md
@@ -0,0 +1,10 @@
+# IMPORTANT NOTES TO GET THIS WORKING
+
+## If not using Void Linux:
+
+Modify the two `X11` library lines in `config.mk` and uncomment the default values
+
+
+## Base README
+
+Found [here](https://git.suckless.org/slstatus/file/README.html)
--- config.mk
+++ config.mk
@@ -7,8 +7,12 @@ VERSION = 0
 PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+# void linux fix
+X11INC = /usr/include/X11
+X11LIB = /usr/lib/X11
+# normally
+# X11INC = /usr/X11R6/include
+# X11LIB = /usr/X11R6/lib
 
 # flags
 CPPFLAGS = -I$(X11INC) -D_DEFAULT_SOURCE
